b1c2c0586bdf11a00b13138a5e23ee906329421d
Javier Arrieta <javier.arrieta@zalando.ie>
2019-09-10 12:53:27 +0100

    Added link to slides

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ We will explore how to build a simple webapp using `Resource` for monadic resour
 We will be using scala 2.13.0, http4s, zio, cats-effect and plain JDBC to see how to interact with legacy libraries.
 
 Link to the slides [here](https://docs.google.com/presentation/d/1DEy_2j9dMoEbwiStHDKd4XUd4GxErlHP0LlDdDSoX64)
+
+## How to run the example app
+You will need to start a database, for instance with docker:
+```bash
+docker run --name postgres -p 5432:5432 -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=customers --rm postgres:alpine
+```
+
+Then import the schema and an example row in the table
+(use `admin` as password as set in the step before):
+
+```
+psql -h localhost -U admin -d customers -f test-data.sql
+```
--- src/main/scala/zalando/meetup/bracket/DatabaseReadsApp.scala
+++ src/main/scala/zalando/meetup/bracket/DatabaseReadsApp.scala
@@ -1,7 +1,6 @@
 package zalando.meetup.bracket
 
 import cats.effect.Resource
-import cats.syntax.either._
 import org.http4s.implicits._
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
@@ -9,7 +8,6 @@ import pureconfig.error.ConfigReaderException
 import zio.console.putStrLn
 import zio.interop.catz._
 import zio.{Task, ZIO}
-import javax.sql.DataSource
 
 object DatabaseReadsApp extends CatsApp {
 
@@ -22,18 +20,12 @@ object DatabaseReadsApp extends CatsApp {
     Resource.make[Task, Unit](Task(println("Starting application")))(_ => Task(println("Resources released, shutting down app")))
 
   override def run(args: List[String]): ZIO[DatabaseReadsApp.Environment, Nothing, Int] = {
-
-    val readDbConfig = Task.fromTry(pureconfig.loadConfig[DatabaseConfig].leftMap(ConfigReaderException.apply).toTry)
-    val httpServerRun = (ds: DataSource) => 
-      BlazeServerBuilder[Task]
-        .bindHttp(8080, "localhost")
-        .withHttpApp(Router("/" -> httpServer(ds, findCustomer[Task])).orNotFound)
-
     val program: Resource[Task, Unit] = for {
       _ <- appLifecycleLogger
-      dbConfig <- Resource.liftF(readDbConfig)
+      dbConfig <- Resource.liftF(Task.fromTry(pureconfig.loadConfig[DatabaseConfig].left.map(ConfigReaderException.apply).toTry))
       ds <- createDataSource[Task](dbConfig)
-      _ <- httpServerRun(ds).resource
+      _ <- BlazeServerBuilder[Task].bindHttp(8080, "localhost")
+              .withHttpApp(Router("/" -> httpServer(ds, findCustomer[Task])).orNotFound).resource
     } yield ()
     program.use(_ => Task.never).catchAll(e => putStrLn(s"Error: ${e.getMessage}") *> Task.succeed(1))
   }
